49af24e744661b664a8a1cf9b3eada4a4ab41825
Julius <47170056+Juwls@users.noreply.github.com>
2023-01-17 15:02:47 +0100

    small name changes for the model

--- base/models.py
+++ base/models.py
@@ -9,18 +9,18 @@ class Category(models.Model):
 
 class Item(models.Model):
     name = models.CharField(max_length = 150)
-    categoryId = models.ForeignKey(Category, verbose_name="fk_category", on_delete=models.CASCADE, null=True)
+    category = models.ForeignKey(Category, verbose_name="fk_category", on_delete=models.CASCADE, null=True)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     description = models.CharField(max_length=150)
 
 class Cart(models.Model):
-    userId = models.ForeignKey(User, verbose_name="fk_user", on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, verbose_name="fk_user", on_delete=models.CASCADE, null=True)
     total = models.DecimalField(max_digits=8, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
 class CartItem(models.Model):
-    cartId = models.ForeignKey(Cart, verbose_name="fk_cart", on_delete=models.CASCADE, null=True)
-    itemId = models.ForeignKey(Item, verbose_name="fk_item", on_delete=models.CASCADE, null=True)
+    cart = models.ForeignKey(Cart, verbose_name="fk_cart", on_delete=models.CASCADE, null=True)
+    item = models.ForeignKey(Item, verbose_name="fk_item", on_delete=models.CASCADE, null=True)
 
 
