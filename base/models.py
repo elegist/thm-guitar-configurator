4a74a25b1951e8928527546c9fd8780b77f2aa64
Robin Patzak <robinpatzak@gmail.com>
2023-01-20 11:01:31 +0100

    added images to configurator buttons

--- base/models.py
+++ base/models.py
@@ -41,6 +41,7 @@ class Item(models.Model):
     category = models.ForeignKey(Category, verbose_name="fk_category", on_delete=models.CASCADE, null=True)
     price = models.DecimalField(max_digits=7, decimal_places=2)
     description = models.CharField(max_length=150)
+    image_path = models.CharField(max_length=200, null=True)
 
     def __str__(self):
         return self.name
