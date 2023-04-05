415999e54bce1ca6be289e51d568608877918c2d
Robin Patzak <robin.patzak@proton.me>
2023-04-05 16:23:29 +0200

    configured static file directories

--- guitar_configurator/settings.py
+++ guitar_configurator/settings.py
@@ -130,15 +130,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'static-root/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    BASE_DIR / 'static/',
 ]
 
-MEDIA_URL = '/images/'
-
-MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_ROOT = BASE_DIR / 'static-root/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
